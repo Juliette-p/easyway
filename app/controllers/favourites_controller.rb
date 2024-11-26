0beb387d5eba39383750c678e5f5705c656ee6db
Lucille Gris <lucille.gris@gmail.com>
2024-11-26 16:17:12 +0100

    ajout favourites view

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,9 @@
 class FavouritesController < ApplicationController
+  def index
+    @favorites = Favourite.where(user: current_user)
+    @activities = []
+    @favorites.each do |favorite|
+      @activities << favorite.activity
+    end
+  end
 end
