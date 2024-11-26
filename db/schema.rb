c447160765c67b3fbfe4eee8a26d923cf85cc155
ran decool <ran.decool@gmail.com>
2024-11-26 15:29:35 +0100

    addDescriptinToActivity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_125426) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_142623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_125426) do
     t.boolean "bike_park"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "favourites", force: :cascade do |t|
