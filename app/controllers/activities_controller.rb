f3c87c8319538bfe5cd1d6ce2bf4b89c95a40fcf
Lucille Gris <lucille.gris@gmail.com>
2024-11-28 15:21:52 +0100

    ajout de la barre de recherche sur la vue index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,24 @@
 class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
   def index
+    # mise à jour de la liste d'activité
     @activities = Activity.all
+    @activities = @activities.search_by_address(params[:address]) if params[:address].present?
+    if params[:stroller].present?
+      @activities = @activities.where(stroller: true) if params[:stroller] == "1"
+    end
+    if params[:changing_table].present?
+      @activities = @activities.where(changing_table: true) if params[:changing_table] == "1"
+    end
+    if params[:high_chair].present?
+      @activities = @activities.where(high_chair: true) if params[:high_chair] == "1"
+    end
+    if params[:kids_friendly].present?
+      @activities = @activities.where(kids_friendly: true) if params[:kids_friendly] == "1"
+    end
+    # raise
+
+    # mise à jour des marqueurs sur la map
     @markers = @activities.geocoded.map do |activity|
       {
         lat: activity.latitude,
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -12,4 +12,11 @@ class Activity < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_address,
+  against: [:address],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
