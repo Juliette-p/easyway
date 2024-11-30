d43211e67b17fe284261bbdf64bdeecbde78170b
Lucille Gris <lucille.gris@gmail.com>
2024-11-30 12:36:10 +0100

    ajout du lien sur le BP ajouter/supprimer de mes favoris

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,9 +1,26 @@
 class FavouritesController < ApplicationController
   def index
-    @favorites = Favourite.where(user: current_user)
+    @favourites = Favourite.where(user: current_user)
     @activities = []
-    @favorites.each do |favorite|
-      @activities << favorite.activity
+    @favourites.each do |favourite|
+      @activities << favourite.activity
     end
+  end
+
+  def create
+    @activity = Activity.find(params[:activity_id])
+    @favourite = Favourite.new(user: current_user, activity: @activity)
+    if @favourite.save
+      redirect_to activity_path(@activity)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @activity = Activity.find(params[:activity_id])
+    @favourite = Favourite.where(user: current_user, activity: @activity).first
+    @favourite.destroy
+    redirect_to activities_path, status: :see_other
   end
 end
