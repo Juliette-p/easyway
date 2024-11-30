63d38434cd97a01f0331893ee4da0a83a48f735d
Lucille Gris <lucille.gris@gmail.com>
2024-11-30 13:35:41 +0100

    formulaire ok sauf bp submit non testé

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -38,4 +38,19 @@ class ActivitiesController < ApplicationController
   def new
     @activity = Activity.new
   end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save!
+      redirect_to activity_path(@activity)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name)
+  end
 end
