73accf90c2c2e894b39e1014d9361ed769a9c1b5
Lucille Gris <lucille.gris@gmail.com>
2024-12-02 09:04:48 +0100

    essai en cours pour la page agenda

--- app/javascript/controllers/agenda_controller.js
+++ app/javascript/controllers/agenda_controller.js
@@ -1,17 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["AgendaView", "BackgroundAgenda"]
+  static targets = ["AgendaView", "BackgroundAgenda", "ActivityShowView"]
 
   connect() {
     console.log("connect agenda ok");
     // console.log(this.fieldBackgroundTarget.style);
     console.log(this.AgendaViewTarget.style);
+    console.log(this.ActivityShowViewTarget.style);
+
   }
 
   hide_agendaView() {
     console.log("hide_agendaView fields_for ok");
     this.AgendaViewTarget.classList.toggle("d-none");
     this.BackgroundAgendaTarget.classList.toggle("d-none");
+
+    // enlever le scroll de la vue show en arrière plan
+    if (this.BackgroundAgendaTarget.classList == "d-none"){
+      this.ActivityShowViewTarget.style = "position: fixed;";
+      this.ActivityShowViewTarget.style = "overflow-y: hidden;";
+
+    } else {
+      this.ActivityShowViewTarget.style = "position: relative;";
+
+    }
   }
 }
