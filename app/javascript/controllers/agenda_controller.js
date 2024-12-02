5aeb52a5399c482e124c3b7fae4bc1e7afeaaf78
Lucille Gris <lucille.gris@gmail.com>
2024-12-02 16:42:49 +0100

    add agenda view

--- app/javascript/controllers/agenda_controller.js
+++ app/javascript/controllers/agenda_controller.js
@@ -1,29 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["AgendaView", "BackgroundAgenda", "ActivityShowView"]
+  static targets = ["AgendaView", "BackgroundAgenda", "ActivityShowView", "calendar"]
 
   connect() {
     console.log("connect agenda ok");
     // console.log(this.fieldBackgroundTarget.style);
-    console.log(this.AgendaViewTarget.style);
-    console.log(this.ActivityShowViewTarget.style);
-
+    // console.log(this.AgendaViewTarget.style);
+    // console.log(this.ActivityShowViewTarget.style);
+    // console.log(this.calendarTarget.innerHTML)
+    // console.log(this.calendarTarget)
+    // class="flatpickr-day selected"
   }
 
   hide_agendaView() {
     console.log("hide_agendaView fields_for ok");
     this.AgendaViewTarget.classList.toggle("d-none");
     this.BackgroundAgendaTarget.classList.toggle("d-none");
+    // console.log(this.calendarTarget.innerHTML);
+    // console.log(this.calendarTarget.childs);
 
-    // enlever le scroll de la vue show en arrière plan
-    if (this.BackgroundAgendaTarget.classList == "d-none"){
-      this.ActivityShowViewTarget.style = "position: fixed;";
-      this.ActivityShowViewTarget.style = "overflow-y: hidden;";
+    // A reprendre : enlever le scroll de la vue show en arrière plan
+    // if (this.BackgroundAgendaTarget.classList == "d-none"){
+    //   this.ActivityShowViewTarget.style = "position: fixed;";
+    //   this.ActivityShowViewTarget.style = "overflow-y: hidden;";
 
-    } else {
-      this.ActivityShowViewTarget.style = "position: relative;";
+    // } else {
+    //   this.ActivityShowViewTarget.style = "position: relative;";
 
-    }
+    // }
+  }
+
+  invitation() {
+    console.log("agenda confirm ok")
+    alert("Une invitation vous a été envoyée par mail.");
   }
 }
