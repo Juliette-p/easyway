3dfa6ddae7ad7b2bb2852815b51212cb516e1b25
Lucille Gris <lucille.gris@gmail.com>
2024-12-04 10:43:14 +0100

    ajout d'un bp de géolocalisation sur mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,6 +20,19 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v12"
     })
 
+    // pour localiser le user
+    this.map.addControl(
+      new mapboxgl.GeolocateControl({
+          positionOptions: {
+              enableHighAccuracy: true
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true
+      })
+    );
+
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
 
