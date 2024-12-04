94fa35004b90d7fabf380e8ec23f5b96799fc36f
Juliette Pokorny <52045086+Juliette-p@users.noreply.github.com>
2024-12-04 16:07:50 +0100

    fait

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -5,12 +5,12 @@ module MetaTagsHelper
   end
 
   def meta_description
-  content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META[“meta_description”]
-  fin
+  content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
+  end
 
   def meta_image
-  meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META[“meta_image”])
+  meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
   # petite modification pour que cela fonctionne aussi bien avec un asset qu'avec une url
-  meta_image.starts_with?(“http”) ? meta_image : image_url(meta_image)
+  meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
-  end
+end
